935c40cbc9384d69ed1e9c98238c16203e4fcf79
SunilJoseK <suniljosek.23eie@kongu.edu>
2025-06-21 09:30:20 +0530

    comlpit.git int

---
+++ day 4/class/module.js
@@ -0,0 +1,6 @@
+export function display(){
+    console.log("Hello from the module!");
+}
+export function add(a, b){
+    return a + b;
+}
---
+++ day 4/class/test.js
@@ -0,0 +1,4 @@
+import {display,add} from './module.js';
+display();
+let result = add(5, 3);
+console.log(result);
